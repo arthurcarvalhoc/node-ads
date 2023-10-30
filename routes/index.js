fb245ab22bb7a40e75510d2f105965d4d6604ae2
Arthur Carvalho <=>
2023-10-30 10:42:56 -0300

    Adicionando laboratorios

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,54 @@
 var express = require('express');
+const { fazerLogin } = require('../repository/usuarioRepositor');
 var router = express.Router();
-const repository = require("../repository/usuarioRepository")
+const labRepository = require('../repository/laboratorioRepository')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('sign-in', { });
 });
 
-router.post('/login', async (req, res, next)=>{
-  console.log(req.body);
+router.get('/laboratorios', function(req, res, next) {
+  res.render('laboratoriosPage', { });
+});
 
-  let { login, senha }= req.body;
+router.post('/createLab', function(req, res, next) {
 
-  if(await repository.getUsuario(login, senha) ){
-    res.render('welcome', {usuario: login})
+  let {nome, capacidade, computadores} = req.body;
+
+  if(labRepository.criarLab(nome, capacidade, computadores)){
+    res.render('laboratoriosPage', { msg: true });
   }else{
-    res.render('error', {message: 'usuário inválido'})
+    res.render('laboratoriosPage', { msg: false });
   }
 
- 
 });
+
+router.post('/autenticar', async function(req, res, next) {
+  let { login, senha } = req.body; 
+
+  console.log( req.body );
+
+  if( await fazerLogin(login, senha)){
+    res.render('home', {  });
+  }else{
+    res.render('sign-in', { erroLogin: true });
+  }
+
+  router.post('/laboratorios', async function(req, res, next) {
+    
+    let { nome, capacidade, computadores } = req.body;
+
+    const repository = require('../repository/laboratorioRepository')
+
+    const success = await repository.criarLab(nome, capacidade, computadores);
+
+    res.render('dashboard', { });
+  });
+  
+
+});
+
 
 
 module.exports = router;
